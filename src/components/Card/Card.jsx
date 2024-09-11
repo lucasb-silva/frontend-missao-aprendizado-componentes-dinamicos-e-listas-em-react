6ff5b905c749a0c4ec5049c427ab255851769974
Lucas Silva <59655760+lucasb-silva@users.noreply.github.com>
2024-09-11 21:21:08 +0000

    Configuração das `props` no componente `<Card />`

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import './Card.css'
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props)
   return (
     <div className="card">
-      <h1>Java</h1>
-      <img src="https://salvatore.academy/devmon/1_java.png" width="200" />
+      <h1>{props.nome}</h1>
+      <img src={props.imagem} width="200" />
     </div>
   )
 }
